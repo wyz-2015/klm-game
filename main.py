033f25c69494a00e7f7eaddf452a29509b4f7cc9
wyz-2015 <1209229723@qq.com>
2024-01-29 00:32:41 +0800

    尝试模块化设计，支持根据KLM按钮信号变化图片。

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from PyQt5.QtWidgets import *
 import os
 
 from core.img_viewer import *
+from core.buttons import *
 
 class Main(QWidget):
     def __init__(self):
@@ -15,9 +16,18 @@ class Main(QWidget):
         print(choice)
         self.img_viewer.play(choice)
 
+        self.btns1=KLM_buttons()
+        self.btns1.signal.connect(self.change_img)
+
         v_layout=QVBoxLayout()
         v_layout.addWidget(self.img_viewer)
+        v_layout.addWidget(self.btns1)
         self.setLayout(v_layout)
+
+    def change_img(self):
+        choice=self.img_viewer.choose_one()
+        print(choice)
+        self.img_viewer.play(choice)
 
 if(__name__=="__main__"):
     app=QApplication(sys.argv)
