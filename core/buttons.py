8552cfdab9b38c32cd0cdcff038decab5899c16c
wyz-2015 <1209229723@qq.com>
2024-01-29 00:29:43 +0800

    新文件。 初步完成了默认状态下的KLM 3个按钮，支持点击与热键发送信号。

---
+++ core/buttons.py
@@ -0,0 +1,65 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+class KLM_buttons(QWidget):
+    signal=pyqtSignal(str)
+    def __init__(self):
+        super(KLM_buttons,self).__init__()
+        #(self.bt_K,self.bt_L,self.bt_M,self.bt_Load,self.bt_Score,self.bt_About)=(QPushButton() for i in range(6))
+
+        #bt_K
+        #self.bt_K.setText("相似(&K)")
+        #self.bt_K.setShortcut(Qt.Key_K)
+        #self.bt_K.clicked.connect(lambda:print("K"))
+        #self.
+
+        #TODO:3个键要能在设置里自定义键位。
+        self.bt_K=Btn("相似(&K)",Qt.Key_K)
+        self.bt_K.signal.connect(lambda:self.signal.emit("K"))
+        self.bt_L=Btn("不同(&L)",Qt.Key_L)
+        self.bt_L.signal.connect(lambda:self.signal.emit("L"))
+        self.bt_M=Btn("特殊(&M)",Qt.Key_M)
+        self.bt_M.signal.connect(lambda:self.signal.emit("M"))
+
+        h_layout=QHBoxLayout()
+        h_layout.addWidget(self.bt_K)
+        h_layout.addWidget(self.bt_L)
+        h_layout.addWidget(self.bt_M)
+
+        self.setLayout(h_layout)
+
+    def keyPressEvent(self,event):#热键发送信号
+        if(event.key()==Qt.Key_K):
+            self.signal.emit("K")
+            print("K")
+        elif(event.key()==Qt.Key_L):
+            self.signal.emit("L")
+            print("L")
+        elif(event.key()==Qt.Key_M):
+            self.signal.emit("M")
+            print("M")
+
+class Btn(QPushButton):#KLM按钮通用模版封装
+    signal=pyqtSignal(bool)
+    def __init__(self,text,hot_key):
+        super(Btn,self).__init__()
+        #self.setText("相似(&K)")
+        self.setText(text)
+        self.hot_key=hot_key
+        #self.setShortcut(Qt.Key_K)
+        #self.setShortcut(hot_key)
+        self.clicked.connect(lambda:self.signal.emit(True))#按下时发射信号
+        
+    #def keyPressEvent(self,event):#热键发射信号
+    #    if(event.key()==self.hot_key):
+    #        self.signal.emit(True)
+
+if(__name__=="__main__"):
+    app=QApplication(sys.argv)
+    #a=Btn("KKKKKK",Qt.Key_K)
+    #a.show()
+    a=KLM_buttons()
+    a.show()
+    sys.exit(app.exec())
